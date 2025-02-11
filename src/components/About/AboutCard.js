7cb6ec8b345d043324ab137a5de8d76e96546cf8
Bharath <163950117+raavibharath@users.noreply.github.com>
2025-02-11 16:13:27 -0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,7 +7,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <p>
-        Experienced Data Feild with a Master of Science in Business Analytics from Saint Peter’s University. Proficient in designing and implementing pipelines, integrating data from diverse sources, and transforming raw data into actionable insights. Skilled in SQL, Azure Data Engineering, and tools like Azure Data Factory, Databricks, and Synapse Analytics for data processing, ETL workflows, and reporting. Certified Azure AI Engineer Associate and Azure Data Engineer Associate, with expertise in business intelligence, cloud-based solutions, and Power BI. Passionate about leveraging data analytics, machine learning, and AI to drive data-driven decision-making and innovation.
+        Experienced in Data Feild with a Master of Science in Business Analytics from Saint Peter’s University. Proficient in designing and implementing pipelines, integrating data from diverse sources, and transforming raw data into actionable insights. Skilled in SQL, Azure Data Engineering, and tools like Azure Data Factory, Databricks, and Synapse Analytics for data processing, ETL workflows, and reporting. Certified Azure AI Engineer Associate and Azure Data Engineer Associate, with expertise in business intelligence, cloud-based solutions, and Power BI. Passionate about leveraging data analytics, machine learning, and AI to drive data-driven decision-making and innovation.
         </p>
         <footer className="blockquote-footer">Bharath Raavi</footer>
       </Card.Body>
