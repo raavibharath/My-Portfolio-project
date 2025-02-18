6ff7aefc793e5f29580405153a955b7fa9ff6933
Bharath <163950117+raavibharath@users.noreply.github.com>
2025-02-18 11:14:56 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/Bharath Raavi.pdf";
+import pdf from "../../Assets/bharath_raavi_ep.pdf";         
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
