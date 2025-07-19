8163edeac8e27ba42260b97484f34c38cdc9c8bc
Bharath <163950117+raavibharath@users.noreply.github.com>
2025-07-19 10:43:09 -0400

    Update Home2.js
    3rd check

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,11 +1,7 @@
 import React from "react";
 // import Particle from "../Particle";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  // AiOutlineTwitter,
-  // AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -19,17 +15,23 @@ function Home2() {
       {/* <Particle /> */}
       <Container className="text-center">
         <Row>
-          <Col md={8} className="home-about-description">
+          <Col md={8} className="home-about-description mx-auto">
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-                <br />
-                Azure & Databricks Specialist: Strategic and detail-oriented Senior Data Engineer with a Master’s in Business Analytics from Saint Peter’s University, bringing deep expertise in designing, developing, and optimizing data pipelines on Azure. Adept at delivering scalable data solutions using Databricks (PySpark/Scala), Delta Lake, and Azure Data Factory, with strong cross-functional understanding of both engineering and analytical workflows.
-                <br />
-                Proficient in SQL, Python, and Spark, with a proven track record of transforming raw data into reliable, production-ready datasets for analytics, reporting, and machine learning. Experienced in implementing robust data architectures using Azure Data Lake Storage, Synapse Analytics, and ADF, enabling real-time insights and data-driven decision-making.
-            </Col>
+              <br />
+              <strong>Azure & Databricks Specialist</strong>: Strategic and detail-oriented Senior Data Engineer with a Master’s in Business Analytics from Saint Peter’s University, bringing deep expertise in designing, developing, and optimizing data pipelines on Azure.
+              <br />
+              <br />
+              Adept at delivering scalable data solutions using Databricks (PySpark/Scala), Delta Lake, and Azure Data Factory, with strong cross-functional understanding of both engineering and analytical workflows.
+              <br />
+              <br />
+              Proficient in SQL, Python, and Spark, with a proven track record of transforming raw data into reliable, production-ready datasets for analytics, reporting, and machine learning. Experienced in implementing robust data architectures using Azure Data Lake Storage, Synapse Analytics, and ADF, enabling real-time insights and data-driven decision-making.
+            </p>
+          </Col>
         </Row>
+
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
@@ -47,7 +49,6 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
-
               <li className="social-icons mx-2">
                 <a
                   href="https://github.com/raavibharath"
@@ -58,27 +59,28 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              {/* <li className="social-icons mx-2">
+              {/* Uncomment if needed:
+              <li className="social-icons mx-2">
                 <a
-                  href="https://twitter.com/sandee_deepu"
+                  href="https://twitter.com/yourhandle"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
-              </li> */}
-              
-              {/* <li className="social-icons mx-2">
+              </li>
+              <li className="social-icons mx-2">
                 <a
-                  href="https://www.instagram.com/sandee_deepu"
+                  href="https://instagram.com/yourhandle"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
                   <AiFillInstagram />
                 </a>
-              </li> */}
+              </li>
+              */}
             </ul>
           </Col>
         </Row>
